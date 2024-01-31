0c71b4071dab1648a658fd0b47e8f02ff6590bed
Bhushan Shahi <bhushanshahi42@gmail.com>
2024-01-31 08:20:24 +0545

    connect postman

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
-const express=require("express");
-const app=express();
-const PORT=8000;
-app.get("/",(req,res)=>{
-    res.json({msg:"hello world"});
+const express = require("express");
+const app = express();
+const PORT = 8000;
+app.use(express.json());
+//read
+app.get("/", (req, res) => {
+  //logic=>db
+  res.json({ msg: "all blog List" });
 });
-app.listen(PORT,()=>{
-    console.log(`app is running on port ${PORT}.Url is http://localhost:${PORT}`);
+//creat
+app.post("/", (req, res) => {
+  const data = req.body;
+  //logic=>send this data to db
+  res.json({ msg: `new blog added data:${data}` });
 });
+//update
+app.put("/id", (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  //logic=>send params with this data to db
+  res.json({ msg: `Blog ID ${id} put with data` });
+});
+//update
+app.patch("/id", (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  //logic=>send params with this data to db
+  res.json({ msg: `Blog ID ${id} put with data` });
+});
+//delete
+app.put("/id", (req, res) => {
+  const { id } = req.params;
+
+  res.json({ msg: `Blog ID ${id} delete` });
+});
+
+app.listen(PORT, () => {
+  console.log(`app is running on port ${PORT}.Url is http://localhost:${PORT}`);
+});
+//data sending mechanism(FE to BF)
+//PARAMS /:id
+//BODY
+//HEADER
