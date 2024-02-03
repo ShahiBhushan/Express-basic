41dbf0eb042b865c321b79e369353397b2d775e2
Bhushan Shahi <bhushanshahi42@gmail.com>
2024-02-03 23:53:54 +0545

    apiVersion update

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,14 @@ const blogRouter=require("../modules/blogs/blog.route");
 const tagRouter=require("../modules/tags/tag.route");
 const roleRouter=require("../modules/roles/role.route");
 const userRouter=require("../modules/users/user.route");
+const apiVersion="/api/v1";
 router.get("/", (req, res) => {
     //logic=>db
     res.json({ msg: "hello from router index" });
   });
-
-router.use("/blogs",blogRouter);
-router.use("roles",roleRouter);
-router.use("/tags",tagRouter);
-router.use("/users",userRouter);
+// api should always have version (v1/v2/v3)
+router.use(`${apiVersion}/roles`,roleRouter);
+router.use(`${apiVersion}/blogs`,blogRouter);
+router.use(`${apiVersion}/tags`,tagRouter);
+router.use(`${apiVersion}/users`,userRouter);
   module.exports=router;
