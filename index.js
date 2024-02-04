8d09fbfd8d0bb3a8460245dfb9013ba503bae49f
Bhushan Shahi <bhushanshahi42@gmail.com>
2024-02-04 09:05:01 +0545

    middleware

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
+require("dotenv").config();
 const express = require("express");
 const indexRouter=require("./routes");
+
 const app = express();
-const PORT = 8000;
+const PORT=Number(process.env.PORT);
+
 app.use(express.json());
+app.use("/assets",express.static("public"));//static files
+//middleware
+app.use((req,res,next)=>{ 
+    req.body.currency="NPR";
+    next();
+   
+});
+// app.use((req,res,next)=>{ 
+//     req.body.country="Nepal";
+//     next();
+// });
 app.use("/",indexRouter);
+//error handling
+app.use((err,req,res,next)=>{
+err=err ? err.toString():"something went wrong";
+res.status(500).json({smg:err});
+});
 app.listen(PORT,()=> {console.log(`app is running on port ${PORT}.Url is http://localhost:${PORT}`);
 });
 //data sending mechanism(FE to BF)
 //PARAMS /:id
 //BODY
-//HEADER
+//HEADERm
--- modules/blogs/blog.route.js
+++ modules/blogs/blog.route.js
@@ -1,21 +1,56 @@
-const router=require("express").Router();
-router.get("/",(req,res)=>{
-    res.json({msg:"hello from blog router"});
+const router = require("express").Router();
+router.get("/", (req, res) => {
+  res.json({ msg: "hello from blog router" });
 });
-router.post("/",(req,res)=>{
-    res.json({msg:"hello from blog  post router"});
+const mw=(req,res,next)=>{
+  req.body.country="Nepal";
+    next();
+};
+//try and catch
+// router.post("/", (req, res, next) => {
+//   try {
+//    // StyleSheetList;
+//     res.json({ msg: "hello from blog  post router" });
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+//or throw error
+router.post("/", mw,(req, res, next) => {
+  try {
+    const { title } = req.body;
+    if (!title) throw new Error("Title is missing");
+    console.log(req.body); //for middleware
+    res.json({ msg: "hello from blog  post router" });
+  } catch (err) {
+    next(err);
+  }
+});
+//finally
+//  router.post("/", (req, res, next) => {
+//     let counter=0;
+//     try {
+//         const {title}=req.body;
+//          if (!title)throw new error("Title is missing");
+//          res.json({ msg: "hello from blog  post router" });
+//        } catch (err) {
+//          next(err);
+//        }
+//        finally{counter ++;
+//     console.log({counter});
+//     }
+//      });
+
+router.put("/:id", (req, res) => {
+  res.json({ msg: "hello from blog put router" });
 });
 
-router.put("/:id",(req,res)=>{
-    res.json({msg:"hello from blog put router"});
+router.patch("/:id", (req, res) => {
+  res.json({ msg: "hello from blog  patch router" });
 });
 
-router.patch("/:id",(req,res)=>{
-    res.json({msg:"hello from blog  patch router"});
+router.delete("/:id", (req, res) => {
+  res.json({ msg: "hello from blog  deleterouter" });
 });
 
-router.delete("/:id",(req,res)=>{
-    res.json({msg:"hello from blog  deleterouter"});
-});
-
-module.exports=router;
+module.exports = router;
